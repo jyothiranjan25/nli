e19c4b48b07077240a307adef5e5a7c271c91cac
jyothiranjan25 <jyothiranjan25@gmail.com>
2022-08-02 23:12:31 +0530

    dash board changes

---
+++ dashboard/includes/footer.php
@@ -0,0 +1,28 @@
+<!--- /footer-top ---->
+<!---copy-right ---->
+<style>
+	.copy-right1 p {
+		color: #fff;
+		font-size: 14px;
+		line-height: 1.8em;
+	}
+
+	.copy-right1 p a {
+		color: #fff;
+	}
+
+	.copy-right1 p a:hover {
+		color: #000000;
+	}
+
+	.copy-right1 {
+		background: #33BEFF;
+		text-align: center;
+		padding: 0.5em 0;
+	}
+</style>
+<div class="copy-right1">
+	<div class="container">
+		<p class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">© 2022 NLI . All Rights Reserved </p>
+	</div>
+</div>
